760b518b380ea801e59892ef4ad9023c224a4c0b
Bhavya Khurana <bhavyakhurana002@gmail.com>
2024-11-11 23:53:11 +0530

    Add Loader

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,5 +1,7 @@
 import { Navbar, NavbarLink } from "@/components/Navbar";
 
+export const dynamic = "force-dynamic";
+
 export default function AdminLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
     return (
         <>
---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import { Loader2 } from "lucide-react";
+
+export default function Adminloading() {
+    return (
+        <div className="h-screen flex justify-center items-center">
+            <Loader2 className="size-24 animate-spin" />
+        </div>
+    )
+}
