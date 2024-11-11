f1b88c4b6da3d73e0409ccdfa706232755f6f38e
Bhavya Khurana <bhavyakhurana002@gmail.com>
2024-11-11 23:54:02 +0530

    Make Navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,9 +5,9 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 
-export function Navbar({ children, }: { children: ReactNode }) {
+export function Navbar({ children }: { children: ReactNode }) {
     return (
-        <nav className="bg-primary text-primary-foreground flex justify-center px-4">
+        <nav className="bg-primary text-primary-foreground flex justify-between sm:justify-center px-4 py-2 md:py-0">
             {children}
         </nav>
     );
@@ -19,7 +19,10 @@ export function NavbarLink(props: Omit<ComponentProps<typeof Link>, "className">
     return (
         <Link
             {...props}
-            className={cn("p-4 hover:bg-secondary hover:text-secondary-foreground focus-visible:bg-secondary focus-visible:text-secondary-foreground", pathname === props.href && "bg-background text-foreground")}
+            className={cn(
+                "p-2 sm:p-4 text-sm sm:text-base hover:bg-secondary hover:text-secondary-foreground focus-visible:bg-secondary focus-visible:text-secondary-foreground",
+                pathname === props.href && "bg-background text-foreground"
+            )}
         />
     );
 }
